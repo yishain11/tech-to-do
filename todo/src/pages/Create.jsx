7519257c9079b18167de9cdac0e6be259803244c
yishain11 <yishain11@walla.co.il>
2023-04-30 15:58:27 +0300

    added clear

--- todo/src/pages/Create.jsx
+++ todo/src/pages/Create.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { createTask } from '../redux/features/task.slice';
 export default function Create() {
     const [taskName, setTaskName] = useState('');
     const [taskDetails, setTaskDetails] = useState('');
+    const f = useRef(null);
     const dispatch = useDispatch();
     return <div>
         <h1>create task</h1>
-        <form onSubmit={(e) => {
+        <form ref={f} onSubmit={(e) => {
             e.preventDefault();
             dispatch(createTask({ name: taskName, details: taskDetails, isComplete: false }));
+            setTaskName('');
+            setTaskDetails('');
+            f.current.reset();
         }}>
             <label htmlFor="taskName">Name</label>
             <input value={taskName} type="text" name='taskName' onInput={(e) => {
