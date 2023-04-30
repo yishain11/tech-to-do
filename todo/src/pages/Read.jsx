7aaf505d688f60ec351608f6dc72cc58c5b3bd59
yishain11 <yishain11@walla.co.il>
2023-04-30 17:30:26 +0300

    added create read tasks using redux rtk

--- todo/src/pages/Read.jsx
+++ todo/src/pages/Read.jsx
@@ -1,18 +1,23 @@
-import { useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { completeTask } from '../redux/features/task.slice';
 export default function Read() {
+    const dispatch = useDispatch();
     const tasks = useSelector((state) => {
-        console.log(state);
         return state;
     });
     return <div>
         <h1>read page</h1>
         <h4>tasks:</h4>
         <div>{tasks.map(task => {
-            return <div key={task.name}>
-                name: {task.name}
-                <br />
-                details: {task.details}
+            return <div key={task.name} style={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{ textDecoration: !task.isComplete ? 'none' : 'line-through' }}>name: {task.name}</div>
+                <div>details: {task.details}</div>
+                <div style={{ display: 'flex', margin: 'auto' }}>
+                    <label htmlFor="isComplete">Is Done?</label>
+                    <input type="checkbox" name="isComplete" id="isComplete" checked={task.isComplete} onChange={(e) => {
+                        dispatch(completeTask({ name: task.name }));
+                    }} />
+                </div>
                 <hr />
             </div>;
         })}</div>
--- todo/src/redux/features/task.slice.js
+++ todo/src/redux/features/task.slice.js
@@ -10,7 +10,14 @@ const tasksSlice = createSlice({
         // updateTask(state, action) { },
         // deleteTask(state, action) { },
         // readTask(state, action) { },
+        completeTask(state, action) {
+            const name = action.payload.name;
+            const taskIndx = state.findIndex(el => el.name === name);
+            const newTask = state[taskIndx];
+            newTask.isComplete = !newTask.isComplete;
+            state[taskIndx] = newTask;
+        }
     },
 });
-export const { createTask } = tasksSlice.actions;
+export const { createTask, completeTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
